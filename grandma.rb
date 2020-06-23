af6eec3db0963fdb6127ddf266dce11cbe00e9d2
William Han Findiesen <william.findiesen@gmail.com>
2020-06-23 00:53:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase = "HELLO")
 
 if phrase == "I LOVE YOU GRANDMA!"
-  return "I lOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
 else
